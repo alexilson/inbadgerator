9ebd8d13d316393fc05a84d83bd5dfa39df0c39a
Alex <ilson.alex@gmail.com>
2023-11-27 17:33:05 -0800

    Created Shapes class, added inquire functionality

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const fs = require('promise/fs');
+const Shape = require('./lib/shapes.js')
+
+// output the text to be saved in the svg file
+// save the svg file named logo.svg
+// output "Generated logo.svg" to the command line
+// 300x200 pixel image
+
+// class Circle extends Shape
+// class Triangle extends Shape
+// class Sqaure extends Shape
+
+const shape = new Shape
+shape.getInput();
